e3c19b1bb94aad96904bae5e842adbffc10022e5
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-16 10:41:49 -0500

    Ejercicio 01: Registro de empleados: Diseñod de la interfas y llamada clase main

--- src/main/java/org/Jhonatann/Ejercicios/app/Principal.java
+++ src/main/java/org/Jhonatann/Ejercicios/app/Principal.java
@@ -1,5 +1,7 @@
 package org.Jhonatann.Ejercicios.app;
 
+import org.Jhonatann.Ejercicios.app.Ejercicio01Empleados.frmEmpleados;
+
 /**
  *
  * @author Jhonatan
@@ -8,5 +10,11 @@ public class Principal {
 
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        ejercicio01();
+    }
+    
+    public static void ejercicio01(){
+        frmEmpleados o = new frmEmpleados();
+        o.setVisible(true);
     }
 }
