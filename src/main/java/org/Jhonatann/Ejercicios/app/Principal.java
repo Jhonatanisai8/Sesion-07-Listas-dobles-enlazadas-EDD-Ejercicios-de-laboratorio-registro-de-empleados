9f64decd30dcf5fede81869ea97f361d8d2b1f6c
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-16 09:47:11 -0500

    Creacion del proyecto

---
+++ src/main/java/org/Jhonatann/Ejercicios/app/Principal.java
@@ -0,0 +1,12 @@
+package org.Jhonatann.Ejercicios.app;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+    }
+}
